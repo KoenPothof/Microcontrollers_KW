4117b0d26d9c91fe229beabd2c32e345d104d059
WesselRo <123960059+WesselRo@users.noreply.github.com>
2024-03-11 09:35:17 +0100

    lcd upgrade

--- 3.B.1/main.c
+++ 3.B.1/main.c
@@ -7,33 +7,34 @@ int main (void){
 	
 	init();
 	set_cursor(2);
-	display_text("NIGGER");
+	display_text("Begin");
 	
 	DDRD = 0b11111111;
 	PORTE = 0x01;
-	PORTF = 0x01;
+	PORTG = 0x01;
 	
 	int i = 0;
 	while (1) {
-		_delay_ms(10);
-		if(PINE == 1){
-			i++;
-			_delay_ms(400);
-			init();
-			display_text("Nikkel 1");
-		}
-		if(PINE == 1 && PIND == 1){
+		_delay_ms(100);
+		if(PINE == 1 && PING == 1){
 			i = 0;
 			_delay_ms(400);
 			init();
-			display_text("Nikkel 3");
-		}
-		if(PIND == 1 && i>5){
-			i--;
+			display_text("e en f ingedrukt");
+			_delay_ms(500);
+		}else if(PINE == 1){
+			i++;
 			_delay_ms(400);
 			init();
-			display_text("Nikkel 2");
-		}
+			display_text("e ingedrukt");
+			_delay_ms(500);
+	}else if(PING == 1 && i > 0){
+		i--;
+		_delay_ms(400);
+		init();
+		display_text("f ingedrukt");
+		_delay_ms(500);
+	}
 		if(i>15){
 			i = 16;
 		}
