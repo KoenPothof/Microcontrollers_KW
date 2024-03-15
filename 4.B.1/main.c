d873b238cd9048605ef8209812789387fcb14fb1
KoenPothof <koenpothof@gmail.com>
2024-03-15 13:19:19 +0100

    goeie

---
+++ 4.B.1/main.c
@@ -0,0 +1,18 @@
+/*
+ * 4.B.1.c
+ *
+ * Created: 3/15/2024 11:21:02
+ * Author : koenp
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
