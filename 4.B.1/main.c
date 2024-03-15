46a4d1a3e9d34f3b7882a5aa54c2c5eb7a3c5bda
KoenPothof <koenpothof@gmail.com>
2024-03-15 13:19:47 +0100

    goeie deel 2

--- 4.B.1/main.c
+++ 4.B.1/main.c
@@ -5,14 +5,34 @@
  * Author : koenp
  */ 
 
+#define F_CPU 8000000UL
 #include <avr/io.h>
+#include <util/delay.h>
 
+void setupADC()
+{
+	DDRF &= ~(1<<PF0); // PORTF.0 input ADC
+	DDRA = 0xFF; // PORTA outputs for displaying ADC value
+	DDRB = 0xFF; // PORTB outputs for displaying ADC value
+	// MUX4..0 = 00000 = ADC0
+	// ADLAR = 1 Left adjust, AVCC with external capacitor at AREF
+	pin
+	ADMUX = 0b01100000;
+	// ADEN: ADC Enable, ADSC: ADC Start Conversion
+	// ADFR: ADC Free Running Select
+	// ADC Prescaler Select Bits : Division Factor = 64
+	ADCSRA = 0b11100110;
+}
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	setupADC();
+	/* Replace with your application code */
+	while (1)
+	{
+		PORTA = ADCL;
+		PORTB = ADCH;
+		_delay_ms(100);
+	}
 }
 
--- tester/main.c
+++ tester/main.c
@@ -5,31 +5,159 @@
  * Author : koenp
  */ 
 
+LCD.h:
+#ifndef MYMATH_H
+#define MYMATH_H
+
+void init();
+void display_text(char *str);
+void display_integer(int num);
+void set_cursor(int position);
+
+#endif
+
+LCD.c:
+#define F_CPU 8e6
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "LCD.h"
 
-// Inclusie van de nodige headers
-#include <util/delay.h> // Voor delay functies
-#include "main.h" // Voor functiedeclaraties en macrodefinities
+#define LCD_E 	3
+#define LCD_RS	2
 
-// Definitie van de main-functie
-int main(void) {
-	// Initialisatie en weergave van de tekst "Dirk" op positie 2 van het LCD-scherm
-	init(); // Initialiseer het LCD-scherm
-	set_cursor(2); // Stel de cursorpositie in op 2
-	display_text("Dirk"); // Toon de tekst "Dirk"
-	_delay_ms(20000); // Wacht 20 seconden
+void lcd_strobe_lcd_e(void);
+void init_4bits_mode(void);
+void lcd_write_string(char *str);
+void lcd_write_data(unsigned char byte);
+void lcd_write_command(unsigned char byte);
+void lcd_clear(void);
 
-	// Initialisatie en weergave van de tekst "Frans" op positie 5 van het LCD-scherm
-	init(); // Initialiseer het LCD-scherm opnieuw (elke keer opnieuw om zeker te zijn van de staat)
-	set_cursor(5); // Stel de cursorpositie in op 5
-	display_text("Frans"); // Toon de tekst "Frans"
-	_delay_ms(20000); // Wacht 20 seconden
+void init() {
+	DDRC = 0xFF;
+	DDRD = 0xFF;
+	init_4bits_mode();
+	init_4bits_mode();
+	lcd_clear();
+}
 
-	// Initialisatie en weergave van de tekst "Geert" op positie 1 van het LCD-scherm
-	init(); // Initialiseer het LCD-scherm opnieuw
-	set_cursor(1); // Stel de cursorpositie in op 1
-	display_text("Geert"); // Toon de tekst "Geert"
-	_delay_ms(20000); // Wacht 20 seconden
+void lcd_strobe_lcd_e(void) {
+	PORTC |= (1<<LCD_E);
+	_delay_ms(1);
+	PORTC &= ~(1<<LCD_E);
+	_delay_ms(1);
+}
 
-	return 1; // Geef 1 terug om aan te geven dat het programma succesvol is afgerond
+void init_4bits_mode(void) {
+	DDRC = 0xFF;
+	PORTC = 0x00;
+
+	PORTC = 0x20;
+	lcd_strobe_lcd_e();
+
+	PORTC = 0x20;
+	lcd_strobe_lcd_e();
+	PORTC = 0x80;
+	lcd_strobe_lcd_e();
+
+	PORTC = 0x00;
+	lcd_strobe_lcd_e();
+	PORTC = 0xF0;
+	lcd_strobe_lcd_e();
+
+	PORTC = 0x00;
+	lcd_strobe_lcd_e();
+	PORTC = 0x60;
+	lcd_strobe_lcd_e();
+}
+
+void display_text(char *str) {
+	for (; *str; str++) {
+		unsigned char byte = *str;
+		// First nibble.
+		PORTC = byte;
+		PORTC |= (1<<LCD_RS);
+		lcd_strobe_lcd_e();
+
+		// Second nibble
+		PORTC = (byte<<4);
+		PORTC |= (1<<LCD_RS);
+		lcd_strobe_lcd_e();
+	}
+}
+
+void display_integer(int num) {
+	char buffer[16]; // Buffer voor de omgezette tekenreeks
+	sprintf(buffer, "%d", num); // Omzetten van integer naar tekenreeks
+	display_text(buffer); // Weergeven van de tekenreeks op het LCD-scherm
+}
+
+void lcd_write_command(unsigned char byte)
+
+{
+	// First nibble.
+	PORTC = byte;
+	PORTC &= ~(1<<LCD_RS);
+	lcd_strobe_lcd_e();
+
+	// Second nibble
+	PORTC = (byte<<4);
+	PORTC &= ~(1<<LCD_RS);
+	lcd_strobe_lcd_e();
+}
+
+void lcd_clear(void) {
+	lcd_write_command(0x01);
+	_delay_ms(2);
+}
+
+void set_cursor(int position) {
+	lcd_write_command(0x80 | (position & 0x7F));
+}
+
+
+Main.c:
+#define F_CPU 8e6
+#include <avr/io.h>
+#include <util/delay.h>
+#include "LCD.h"
+
+int main (void){
+	
+	DDRD = 0b11111111;
+	PORTE = 0x01;
+	PORTG = 0x01;
+	
+	init(); //initialiseer lcd
+	set_cursor(5);
+	
+	int i = 0;
+	display_integer(i); //zet alvast de 0 op het LCD scherm
+	
+	while (1) {
+		_delay_ms(100);
+		if(PINE == 1 && PING == 1){
+			while(PINE == 1 && PING == 1){ //wacht totdat te knoppen los worden gelaten
+				_delay_ms(10);
+			}
+			i = 0;
+			init(); //reset lcd
+			display_integer(i); //display i
+			}else if(PINE == 1){
+			while(PINE == 1){ //wacht totdat de knop wordt losgelaten
+				_delay_ms(10);
+			}
+			i++;
+			init(); //reset lcd
+			display_integer(i); //display i
+			}else if(PING == 1 && i > 0){ //zorg ervoor dat de i niet in de min kan
+			while(PING == 1){ //wacht totdat de knop wordt losgelaten
+				_delay_ms(10);
+			}
+			i--;
+			init(); //reset lcd
+			display_integer(i); //display i
+		}
+	}
+	return 1;
 }
