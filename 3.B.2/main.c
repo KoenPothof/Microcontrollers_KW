9b5e725964df464fc42ddb1f9b817a2ee6e68b12
Wessel <w.roovers1@student.avans.nl>
2024-03-27 12:09:54 +0100

    Week 3 af

--- 3.B.2/main.c
+++ 3.B.2/main.c
@@ -1,31 +1,18 @@
 #include <avr/io.h>
-#include <avr/interrupt.h>
 
-volatile uint8_t pulse_state = 0; // Variabele om de staat van de puls bij te houden
+int main(void) {
+	DDRD |= (1 << 7); // Set LED as output
+	TCCR1B |= (1 << CS10); // Set up timer
 
-ISR(TIMER2_COMPA_vect) {
-	if (pulse_state == 0) {
-		// Pulshoogte van 15 ms
-		PORTD |= (1 << PD7); // Zet pin PD7 hoog
-		OCR2A = 585; // 15 ms bij een prescaler van 64 (250 * 64 * 1/16e6 = 0.015)
-		pulse_state = 1;
-		} else {
-		// Pulslage van 25 ms
-		PORTD &= ~(1 << PD7); // Zet pin PD7 laag
-		OCR2A = 976; // 25 ms bij een prescaler van 64 (250 * 64 * 1/16e6 = 0.025)
-		pulse_state = 0;
+	for (;;) {
+		// Check timer value in if statement, true when count matches 15 ms
+		if (TCNT1 >= 15000 && TCNT1 < 40000) {
+			PORTD &= ~(1 << 7); // Turn the LED off
+			} else if (TCNT1 >= 40000) {
+			PORTD |= (1 << 7); // Turn the LED on
+			TCNT1 = 0; // Reset timer value
+		}
 	}
-}
 
-int main() {
-	DDRD |= (1 << PD7); // Zet PD7 als uitgang
-	TCCR2A = (1 << WGM21); // CTC-modus
-	TCCR2A = (1 << CS22); // Prescaler 64
-	TIMSK2 = (1 << OCIE2A); // Timer/Counter2 Output Compare Match A Interrupt Enable
-	sei(); // Globale interrupts inschakelen
-	OCR2A = 585; // Initialiseer met 15 ms
-	while (1) {
-		// Blijf in een oneindige lus
-	}
 	return 0;
 }
