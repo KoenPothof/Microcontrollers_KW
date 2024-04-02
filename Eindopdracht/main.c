7c638b0abee7b7f971e9ae2300e18f2136be1e62
Koen <k.pothof@student.avans.nl>
2024-04-02 13:25:17 +0200

    Eindopdracht finish :)

--- Eindopdracht/main.c
+++ Eindopdracht/main.c
@@ -3,23 +3,21 @@
 #include <util/delay.h>
 
 #define BIT(x)		( 1<<x )
-#define DDR_SPI		DDRB					// spi Data direction register
-#define PORT_SPI	PORTB					// spi Output register
-#define SPI_SCK		1						// PB1: spi Pin System Clock
-#define SPI_MOSI	2						// PB2: spi Pin MOSI
-#define SPI_MISO	3						// PB3: spi Pin MISO
-#define SPI_SS		0						// PB0: spi Pin Slave Select
+#define DDR_SPI		DDRB					// SPI-data richtingsregister
+#define PORT_SPI	PORTB					// SPI-uitvoerregister
+#define SPI_SCK		1						// PB1: SPI-pin Systeemklok
+#define SPI_MOSI	2						// PB2: SPI-pin MOSI
+#define SPI_MISO	3						// PB3: SPI-pin MISO
+#define SPI_SS		0						// PB0: SPI-pin Slave Select
 
 #define PASS_THRESHOLD 20
 
-#define TRIGGER_PIN 1     // The GPIO pin connected to the trigger pin of the ultrasonic sensor
-#define ECHO_PIN 0        // The GPIO pin connected to the echo pin of the ultrasonic sensor
+#define TRIGGER_PIN 1     // De GPIO-pin verbonden met de trigger-pin van de ultrasone sensor
+#define ECHO_PIN 0        // De GPIO-pin verbonden met de echo-pin van de ultrasone sensor
 #define LED_PIN PA0
 
 double seconden = 0;
 
-// wait(): busy waiting for 'ms' millisecond
-// used library: util/delay.h
 void wait(int ms)
 {
 	for (int i=0; i<ms; i++)
@@ -30,86 +28,59 @@ void wait(int ms)
 
 void spi_masterInit(void)
 {
-	DDR_SPI = 0xff;							// All pins output: MOSI, SCK, SS, SS_display as output
-	DDR_SPI &= ~BIT(SPI_MISO);				// 	except: MISO input
-	PORT_SPI |= BIT(SPI_SS);				// SS_ADC == 1: deselect slave
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR1);	// or: SPCR = 0b11010010;
-	// Enable spi, MasterMode, Clock rate fck/64, bitrate=125kHz
-	// Mode = 0: CPOL=0, CPPH=0;
+	DDR_SPI = 0xff;							// Alle pinnen output: MOSI, SCK, SS, SS_display als output
+	DDR_SPI &= ~BIT(SPI_MISO);				// Behalve: MISO input
+	PORT_SPI |= BIT(SPI_SS);				// SS_ADC == 1: deselecteer slave
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR1);	// of: SPCR = 0b11010010;
 }
 
-
-// Write a byte from master to slave
 void spi_write( unsigned char data )
 {
-	SPDR = data;							// Load byte to Data register --> starts transmission
-	while( !(SPSR & BIT(SPIF)) ); 			// Wait for transmission complete
+	SPDR = data;							// Laad byte naar Data-register --> start transmissie
+	while( !(SPSR & BIT(SPIF)) ); 			// Wacht op voltooide transmissie
 }
 
-// Write a byte from master to slave and read a byte from slave
-// nice to have; not used here
-char spi_writeRead( unsigned char data )
-{
-	SPDR = data;							// Load byte to Data register --> starts transmission
-	while( !(SPSR & BIT(SPIF)) ); 			// Wait for transmission complete
-	data = SPDR;							// New received data (eventually, MISO) in SPDR
-	return data;							// Return received byte
-}
-
-// Select device on pinnumer PORTB
 void spi_slaveSelect(unsigned char chipNumber)
 {
 	PORTB &= ~BIT(chipNumber);
 }
 
-// Deselect device on pinnumer PORTB
 void spi_slaveDeSelect(unsigned char chipNumber)
 {
-
 	PORTB |= BIT(chipNumber);
 }
 
 void spi_writeWord (unsigned char adress, unsigned char data){
-	spi_slaveSelect(0); // selecteer display chip
-	spi_write(adress); // verstuur adres
-	spi_write(data); // verstuur data
-	spi_slaveDeSelect(0); // deselecteer display chip
+	spi_slaveSelect(0); // Selecteer display chip
+	spi_write(adress); // Verstuur adres
+	spi_write(data); // Verstuur data
+	spi_slaveDeSelect(0); // Deselecteer display chip
 }
 
-// Initialize the driver chip (type MAX 7219)
 void displayDriverInit()
 {
-	spi_writeWord(0x09, 0xFF);		// 09: Decode Mode -> 1's = BCD mode for all digits
-	
+	spi_writeWord(0x09, 0xFF);		// 09: Decode Mode -> 1's = BCD mode voor alle cijfers
 	spi_writeWord(0x0A, 0x0F);		// 0A: Helderheid -> level F (1 t/m F)
-	
 	spi_writeWord(0x0B, 0x0F);		// 0B: Scan-limiet -> Digits 0 t/m 3
-	
 	spi_writeWord(0x0C, 0x01);		// 0C: Afsluiten -> 1 = normaal
 }
 
-// Set display on ('normal operation')
 void displayOn()
 {
 	spi_writeWord(0x0C, 0x01); // Afsluiten -> 1 = normaal
 }
 
-// Set display off ('shut down')
 void displayOff()
 {
 	spi_writeWord(0x0C, 0x00); // Afsluiten -> 1 = normaal
 }
 
 void writeLedDisplay(int value){
-	
-	// alle digits op -1 zodat je straks kan kijken of deze veranderd worden
 	int digit1 = -1;
 	int digit2 = -1;
 	int digit3 = -1;
 	char digit4 = -1;
 	
-	
-	// aan de hand van de value worden de cijfers gesplitst in de digits, bij negatieve value worden de cijfers een digit opgeschoven en komt er een min-teken voor
 	if (value < 10 && value >= 0){
 		digit4 = value;
 		} else if (value >= 10 && value < 100){
@@ -125,23 +96,22 @@ void writeLedDisplay(int value){
 		digit2 = (value / 10) % 10;
 		digit1 = value % 10;
 		}  else if (value < 0 && value > -10){
-		digit4 = 0x0F; // hier moet eigenlijk een - teken staan
+		digit4 = 0x0F; // Hier moet eigenlijk een - teken staan
 		value = (value * -1);
 		digit3 = value;
 		} else if (value <= -10 && value > -100){
-		digit4 = 0x0F; // hier moet eigenlijk een - teken staan
+		digit4 = 0x0F; // Hier moet eigenlijk een - teken staan
 		value = (value * -1);
 		digit3 = value / 10;
 		digit2 = value % 10;
 		} else if (value <= -100 && value > -1000){
-		digit4 = 0x0F; // hier moet eigenlijk een - teken staan
+		digit4 = 0x0F; // Hier moet eigenlijk een - teken staan
 		value = (value * -1);
 		digit3 = value / 100;
 		digit2 = (value / 10) % 10;
 		digit1 = value % 10;
 	}
 	
-	// check of de digits zijn veranderd en print ze dan
 	if (digit1 != -1){
 		spi_writeWord(1, digit1);
 		_delay_ms(10);
@@ -173,89 +143,85 @@ void writeLedDisplay(int value){
 }
 
 void initUltrasonic() {
-	DDRD |= (1 << TRIGGER_PIN); // Set trigger pin as output
-	DDRD &= ~(1 << ECHO_PIN);   // Set echo pin as input
+	DDRD |= (1 << TRIGGER_PIN); // Zet trigger-pin als output
+	DDRD &= ~(1 << ECHO_PIN);   // Zet echo-pin als input
 }
 
 void triggerPulse() {
-	PORTD |= (1 << TRIGGER_PIN); // Set trigger pin high
-	_delay_us(10);               // Wait for 10 microseconds
-	PORTD &= ~(1 << TRIGGER_PIN);// Set trigger pin low
+	PORTD |= (1 << TRIGGER_PIN); // Zet trigger-pin hoog
+	_delay_us(10);               // Wacht 10 microseconden
+	PORTD &= ~(1 << TRIGGER_PIN);// Zet trigger-pin laag
 }
 
 uint16_t measureDistance() {
 	uint16_t pulse_width = 0;
-	uint32_t timeout = 10000; // Set a timeout value (adjust as needed)
+	uint32_t timeout = 10000; // Zet een time-out waarde (aanpassen indien nodig)
 
-	// Wait for echo pin to go high
+	// Wacht tot echo-pin hoog wordt
 	while (!(PIND & (1 << ECHO_PIN)) && timeout > 0) {
 		timeout--;
 		_delay_us(2);
 	}
 
-	// Measure pulse width
+	// Meet pulsbreedte
 	while ((PIND & (1 << ECHO_PIN)) && timeout > 0) {
 		pulse_width++;
 		_delay_us(2);
 	}
 
-	return pulse_width; // Return the pulse width
+	return pulse_width; // Geef de pulsbreedte terug
 }
 
-// LED aan
 void ledOn(){
-	PORTA |= (1 << LED_PIN); // zet de LED aan
+	PORTA |= (1 << LED_PIN); // Zet de LED aan
 }
 
-// LED uit
 void ledOff(){
-	PORTA &= ~(1 << LED_PIN); // zet de LED uit
+	PORTA &= ~(1 << LED_PIN); // Zet de LED uit
 }
 
 void timerStart(){
-	 PORTC = 0x01;
-	 DDRE = 0x0F;
+	PORTC = 0x01;
+	DDRE = 0x0F;
 
-	 TCCR1B |= ((1 << CS10 ) | (1 << CS11 )); // Set timer Fcpu/64
+	TCCR1B |= ((1 << CS10 ) | (1 << CS11 )); // Zet timer op Fcpu/64
 
-	 for (;;) {
-		 if (TCNT1 >= 62496) {
-			 TCNT1 = 0; // Reset timer
-			 seconden += 0.5; // You should initialize 'seconden' somewhere
-			 writeLedDisplay(seconden);
-		 }
+	for (;;) {
+		if (TCNT1 >= 62496) {
+			TCNT1 = 0; // Reset timer
+			seconden += 0.5; // Initialiseer 'seconden' ergens
+			writeLedDisplay(seconden);
+		}
 
-		 triggerPulse(); // Send trigger pulse
+		triggerPulse(); // Stuur trigger-puls
 
-		 uint16_t pulse_width = measureDistance(); // Measure echo pulse width
+		uint16_t pulse_width = measureDistance(); // Meet echo-pulsbreedte
 
-		 uint16_t distance = pulse_width/58; // Convert pulse width to distance in cm
+		uint16_t distance = pulse_width/58; // Converteer pulsbreedte naar afstand in cm
 
 		if (seconden==2){
 			ledOff();
 		}
-		 if (distance < 10) { // Threshold distance
-			 PORTE = 0b00000000; // Turn off LEDs
-			 seconden = 0; // Reset the counter
-			 ledOn();
-			 
-			 } else {
-			 PORTE = 0b11111111; // Turn on LEDs
-		 }
-	 }
+		if (distance < 10) { // Drempelafstand
+			PORTE = 0b00000000; // Zet LEDs uit
+			seconden = 0; // Reset de teller
+			ledOn();
+			
+			} else {
+			PORTE = 0b11111111; // Zet LEDs aan
+		}
+	}
 }
 
 int main()
 {
 	DDRA |= (1 << LED_PIN);
-	// inilialize
-	spi_masterInit();              	// Initialize spi module
-	displayDriverInit();            // Initialize display chip
+	// Initialiseer
+	spi_masterInit();              	// Initialiseer spi-module
+	displayDriverInit();            // Initialiseer display-chip
 	initUltrasonic();
 	timerStart();
-		
+	
 	
 	return (1);
 }
-
-
